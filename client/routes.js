a19e86600554502adeec258c635e8def100536d9
lucyrichards <lucyannerichards@gmail.com>
2014-07-09 22:44:32 -0700

    added all of the files to ensure that nick and I are totally up to date with all changeS

---
+++ client/routes.js
@@ -0,0 +1,57 @@
+
+/**----------------------- CONFIGURATIONS ---------------------- **/
+
+Router.configure({
+   layoutTemplate: 'layout',  //can be any template name
+   loadingTemplate: 'loading'
+});
+
+
+/**----------------------- ROUTES ---------------------- **/
+
+Router.map(function() { 
+
+  this.route('home', {
+    path: '/',
+    layoutTemplate: 'homelayout',
+    yieldTemplates:{
+      'home':{to: 'content'},
+    }
+  });
+
+  this.route('explore', {
+    path: '/explore',
+    yieldTemplates:{
+      'explore':{to: 'content'},
+    }
+  });
+
+  this.route('campaign', {
+    path: '/campaign',
+    yieldTemplates:{
+      'campaign':{to: 'content'},
+    }
+  });
+
+  this.route('extras', {
+    path: '/extras',
+    yieldTemplates:{
+      'extras':{to: 'content'},
+    }
+  });
+
+  this.route('howitworks', {
+    path: '/howitworks',
+    yieldTemplates:{
+      'howitworks':{to: 'content'},
+    }
+  });
+
+  this.route('login', {
+    path: '/login',
+    yieldTemplates:{
+      'login':{to: 'content'},
+    }
+  });
+  
+});
